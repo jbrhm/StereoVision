0bb5d84100df34e8a7379671d9d2bd0084ad1cb6
John <jrabraham1904@gmail.com>
2024-02-29 13:50:39 -0500

    ZED Gradient Stream

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
+#include <cstdlib>
 #include <iostream>
+#include <unistd.h>
 #include <opencv4/opencv2/opencv.hpp>
 #include <opencv4/opencv2/highgui.hpp>
 #include "Gradient.hpp"
@@ -7,7 +9,6 @@
 
 
 using namespace sl;
-using namespace std;
 
 int main() {
     Camera zed;
@@ -20,54 +21,56 @@ int main() {
     // Open the camera
     auto returned_state = zed.open(init_parameters);
     if (returned_state != ERROR_CODE::SUCCESS) {
-        cout << "Error " << returned_state << ", exit program." << endl;
+		std::cout << "Error " << returned_state << ", exit program." << std::endl;
         return EXIT_FAILURE;
     }
 
     // Capture 50 frames and stop
     int i = 0;
     sl::Mat image;
-    while (i < 50) {
+    while (true) {
         // Grab an image
         returned_state = zed.grab();
         // A new image is available if grab() returns ERROR_CODE::SUCCESS
         if (returned_state == ERROR_CODE::SUCCESS) {
 
             // Get the left image
-            zed.retrieveImage(image, VIEW::LEFT);
-			cv::Mat cvMat = MatHelper::slMat2cvMat(image);
-			cv::imshow("ZED", cvMat);
-			cv::waitKey();
-            
+			zed.retrieveImage(image, VIEW::LEFT);
+			cv::Mat cvImage = MatHelper::slMat2cvMat(image);
+			cv::Mat gray;
+			cv::cvtColor(cvImage, gray, 6);
+			cv::Mat grayScale = Gradient(gray).getGradient();
             // Display the image resolution and its acquisition timestamp
-            cout<<"Image resolution: "<< image.getWidth()<<"x"<<image.getHeight() <<" || Image timestamp: "<<image.timestamp.data_ns<<endl;
-            i++;
-        }
+			i++;
+        	std::cout << "NUM INTERATIONS: " << i << std::endl;
+		}
+		else{
+			return EXIT_FAILURE;
+		}
     }
 
     // Close the camera
     zed.close();
     return EXIT_SUCCESS;;
 
-
-	//cv::Mat image = cv::imread("../dog.jpeg")// Create a ZED camera object
-	//cv::imshow("dawg", image);
-	//cv::waitKey();
-
-	//cv::Mat grayScale{image.rows, image.cols, 0, cv::Scalar{0,0,0}};
-	//cv::cvtColor(image, grayScale, 6);
-
-	//cv::imshow("gray", grayScale);
-	//cv::waitKey();
-
-	//cv::Mat grad = Gradient{grayScale}.getGradient();
-	//std::cout << grad.depth() << std::endl;
-	//MatHelper::normalizeImage(grad, static_cast<uchar>(0), static_cast<uchar>(255));
-
-	//cv::imshow("DOG IMAGE", grad);
-	//cv::waitKey();
-	//std::cout << "Hello, World" << std::endl;
-	//return 0;
+//	cv::Mat image = cv::imread("../dog.jpeg")// Create a ZED camera object
+//	cv::imshow("dawg", image);
+//	cv::waitKey();
+//
+//	cv::Mat grayScale{image.rows, image.cols, 0, cv::Scalar{0,0,0}};
+//	cv::cvtColor(image, grayScale, 6);
+//
+//	cv::imshow("gray", grayScale);
+//	cv::waitKey();
+//
+//	cv::Mat grad = Gradient{grayScale}.getGradient();
+//	std::cout << grad.depth() << std::endl;
+//	MatHelper::normalizeImage(grad, static_cast<uchar>(0), static_cast<uchar>(255));
+//
+//	cv::imshow("DOG IMAGE", grad);
+//	cv::waitKey();
+//	std::cout << "Hello, World" << std::endl;
+//	return 0;
 }
 
 
